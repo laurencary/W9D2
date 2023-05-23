99d034b608c2f75011ad9bd41bee85479441a161
Your Name <129026648+Vneet11@users.noreply.github.com>
2023-05-23 14:24:51 -0700

    drew grid

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,12 +1,12 @@
 // Add your import statements for View and Game here
-import View from './ttt-view';
-import Game from './game.js';
+import View from './ttt-view.js';
+import Game from '../ttt_node/game.js';
 
 document.addEventListener("DOMContentLoaded", () => {
   const el = document.querySelector('.ttt');
   
   const game = new Game();
   const view = new View(game, el);
-
+  
   
 });
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -2,12 +2,24 @@ class View {
   constructor(game, el) {
     this.game = game;
     this.el = el;
+    this.set = this.setupBoard();
+    console.log(this.set)
+    this.el.appendChild(this.set);
   }
   
   setupBoard() {
     let grid = document.createElement('ul');
-    
-  }
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        let box = document.createElement('li');
+        box.setAttribute("data-pos", [row, col]);
+        grid.appendChild(box);
+        
+      };
+     
+    };
+    return grid;
+  };
   
   handleClick(e) {
   }
